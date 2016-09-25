a887598a06d34dc4792118e29facbe963ed44b5f
valykhin <ivalykhin@gmail.com>
2016-09-25 07:52:41 +0600

    some changes

--- src/test/java/org/hometest/test/OperationTestSteps.java
+++ src/test/java/org/hometest/test/OperationTestSteps.java
@@ -13,22 +13,22 @@ public class OperationTestSteps {
 
     @Step("Check adding operation result")
     public static void addTest(final String operand1, final String operand2, final String result) {
-        int expectedResult = Integer.parseInt(result);
-        int actualResult = Integer.parseInt(operand1) + Integer.parseInt(operand2);
+        final int expectedResult = Integer.parseInt(result);
+        final int actualResult = Integer.parseInt(operand1) + Integer.parseInt(operand2);
         Assert.assertEquals(expectedResult, actualResult);
     }
 
     @Step("Check subtracting operation result")
     public static void subtractTest(final String operand1, final String operand2, final String result) {
-        int expectedResult = Integer.parseInt(result);
-        int actualResult = Integer.parseInt(operand1) - Integer.parseInt(operand2);
+        final int expectedResult = Integer.parseInt(result);
+        final int actualResult = Integer.parseInt(operand1) - Integer.parseInt(operand2);
         Assert.assertEquals(expectedResult, actualResult);
     }
 
     @Step("Check multiplying operation result")
     public static void multiplyTest(final String operand1, final String operand2, final String result) {
-        int expectedResult = Integer.parseInt(result);
-        int actualResult = Integer.parseInt(operand1) * Integer.parseInt(operand2);
+        final int expectedResult = Integer.parseInt(result);
+        final int actualResult = Integer.parseInt(operand1) * Integer.parseInt(operand2);
         Assert.assertEquals(expectedResult, actualResult);
     }
 
@@ -36,7 +36,7 @@ public class OperationTestSteps {
     public static void divisionTest(final String operand1, final String operand2, final String result) {
         final double expectedResult = Double.parseDouble(result);
         try {
-            double actualResult = Double.parseDouble(operand1) / Double.parseDouble(operand2);
+            final double actualResult = Double.parseDouble(operand1) / Double.parseDouble(operand2);
             Assert.assertEquals(expectedResult, actualResult, 0.0000001);
         } catch (ArithmeticException ex) {
             Assert.assertTrue(ex.getMessage(), false);
